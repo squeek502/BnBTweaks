86b7734ac51624224a964ef1121f37b773a62d21
Blargerist <yaywarcraft@gmail.com>
2014-06-22 20:10:30 -0600

    Update ClassTransformer.java
    Added code to stop HostileWorlds from spawning the boss pyramid.

--- java/eyamaz/bnbtweaks/asm/ClassTransformer.java
+++ java/eyamaz/bnbtweaks/asm/ClassTransformer.java
@@ -1,6 +1,7 @@
 package eyamaz.bnbtweaks.asm;
 
 import static org.objectweb.asm.Opcodes.*;
+import java.util.Iterator;
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
@@ -30,6 +31,20 @@ public class ClassTransformer implements IClassTransformer
 				fixExtraTiCMelting(methodNode);
 			}
 
+			return writeClassToBytes(classNode);
+		}
+		
+		if (name.equals("hostileworlds.dimension.gen.MapGenSchematics"))
+		{
+			ModBnBTweaks.Log.info("Patching HostileWorld's MapGenSchematis....");
+			
+			ClassNode classNode = readClassFromBytes(bytes);
+			MethodNode methodNode = findMethodNodeOfClass(classNode, "genTemple", "(Lnet/minecraft/world/World;II[B)V");
+			if (methodNode != null)
+			{
+				fixHostileWorldsMapGenSchematics(methodNode);
+			}
+			
 			return writeClassToBytes(classNode);
 		}
 
@@ -101,5 +116,28 @@ public class ClassTransformer implements IClassTransformer
 
 		ModBnBTweaks.Log.info(" Patched " + method.name);
 	}
-
+	
+	public void fixHostileWorldsMapGenSchematics(MethodNode method)
+	{
+		Iterator<AbstractInsnNode> iter = method.instructions.iterator();
+		
+		AbstractInsnNode currentNode = null;
+		int index = -1;
+		
+		//Iterate through all instructions within method
+		//Delete all instructions without Opcode -1 or 177
+		
+		while (iter.hasNext())
+		{
+			index++;
+			currentNode = iter.next();
+			
+			if (currentNode.getOpcode() != -1 && currentNode.getOpcode() != 177)
+			{
+				method.instructions.remove(currentNode);
+			}
+		}
+		
+		ModBnBTweaks.Log.info(" Patched " + method.name);
+	}
 }
